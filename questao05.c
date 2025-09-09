39950d2f1094fc69f1b912cb3d9c83a5deed7b83
Judysonandrade <andradejudyson@gmail.com>
2025-09-09 13:01:16 -0300

    Atualizando

--- questao05.c
+++ questao05.c
@@ -2,15 +2,30 @@
  #include <stdio.h>
  #include <stdlib.h>
 
- void funcao(char** str){
-    str++;
+ void funcao(char** str){ // função tem como parametro um ponteiro para ponteiro de char na variavel str
+    // a variavel str que esta apontando para o endereço do ponteiro str incrementa 1 posição
+    while(**str != NULL){
+          // so da movento o ponteiro para frente, mas não altera o valor do ponteiro str na main
+        printf("%s\n", *str); 
+        str++;
+    } // enquanto o valor apontado por str for diferente de '\0' (final da string  )
+    //str++; // so da movento o ponteiro para frente, mas não altera o valor do ponteiro str na main
+    //printf("%s\n", *str); // imprime a string armazenada no endereço de memoria apontado por str
+    // A função função não altera o valor do ponteiro str na função main, pois a função recebe uma cópia do endereço do ponteiro str
+    // e o incremento de str++ altera apenas essa cópia local, não afetando o ponteiro original na main.
+    // para alterar o valor do ponteiro str na main, seria necessário usar *str++ ou similar para modificar o conteúdo apontado pelo ponteiro.
+
+    //(*str)++; 
+    //printf("%s\n", *str); // imprime a string armazenada no endereço de memoria apontado por str
 }
 
  int main(){
-    char *str = (void *)malloc(50*sizeof(char));
-    strcpy(str, "Agostinho");
-    funcao(&str);
-    puts(str);
-    free(str);
+    char *str = (void *)malloc(50*sizeof(char)); // essa linha faz uma alocação de memoria para 50 caracteres do tipo char
+    strcpy(str, "Agostinho"); // copia a string "Agostinho" para o endereço de memoria apontado por str
+    funcao(&str); // passa o endereço do ponteiro str para a função funcao
+    puts(str); // imprime a string armazenada no endereço de memoria apontado por str
+    free(str); // libera a memoria alocada para str para não acontecer um memory leak
+    
+
     return 0;
 }
