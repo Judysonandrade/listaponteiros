cb4888ce7b025e6bf4f26f048b627a98043cc5bc
Judysonandrade <andradejudyson@gmail.com>
2025-09-10 20:42:19 -0300

    Atualizando

--- questao10.c
+++ questao10.c
@@ -6,13 +6,28 @@ int main(){
 
     for(i=0;i<3;i++){
         printf("%d ", *(vet+i));
-    
+
+        // Esse laço faz a leitura dos numeros nas posições do vet
+        // vet[0] = 4; vet[1] = 9; vet[2] = 13
+        // o laço ele percorre pelas posições do vetor e em vet+i ele pega o endereço do primeiro elemento e incrementa
+        // em i, em que i é um inteiro de 4 bytes ou seja ao incrementa ele ira ter o endereço do segundo elemento e assim vai para 
+        // para o terceiro elemento. E para acessar o valor desse endereços usa-se o *, logo a saida vai ser 4, 9, 13
+        // Saida para v[0] = *(61FF10) = 4 
+        // saida para v[1] = *(61FF10 + 4) = *(61FF14) = 9
+        // saida para v[2] = *(61FF14 + 4) = *(61FF18) = 13
     }
 
     printf("\n");
 
     for(i=0; i<3;i++){
-        printf("%X\n", vet+i);
+        printf("%X\n", vet+i); 
+        
+        // esse for tem a função de percorrer o endereço de memoria dos elementos do vetor
+        // logo vet vai acessar o primeiro endereço do vet e vai encrementar em i que como o tipo
+        // é inteiro ele vai de 4 a 4 até i < 3
+        // Saida para v[0] = 61FF10 
+        // saida para v[1] = 61FF10 + 4 = 61FF14
+        // saida para v[2] = 61FF14 + 4 = 61FF18
     }
 
     return 0;
---
+++ questao11.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*/
+struct teste{
+    int x = 3; // não pode ser inicializado  nada no struct
+    char nome[] = "Jose"; // não pode ser inicializado no struct
+
+};
+
+int main(){
+    struct teste *s; // s é um ponteiro, mas NÃO foi inicializado
+    printf("%d", s->x); // acessando memória através de ponteiro não inicializado
+    printf("%s", s->nome); // acessando memória através de ponteiro não inicializado
+}
+
+*/
+
+struct teste{
+    int x;
+    char nome[20];
+
+};
+
+int main(){
+    struct teste s; // Declaração de um tipo de struct S
+
+    s.x = 3;
+
+    strcpy(s.nome, "Jose");
+
+    printf("%d\n", s.x);
+
+    printf("%s\n", s.nome);
+
+
+    return 0;
+}
