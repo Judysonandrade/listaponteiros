a29c43f26a8beda5d1571fbddfb9f2456aec01f5
Judysonandrade <andradejudyson@gmail.com>
2025-09-10 20:20:15 -0300

    Atualizado

---
+++ questao10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int vet [] = {4, 9, 13};
+    int i;
+
+    for(i=0;i<3;i++){
+        printf("%d ", *(vet+i));
+    
+    }
+
+    printf("\n");
+
+    for(i=0; i<3;i++){
+        printf("%X\n", vet+i);
+    }
+
+    return 0;
+}
