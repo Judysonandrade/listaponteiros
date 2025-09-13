0812c3fdd3776c0f816c5b57f4c1cd06e74a9e78
Judysonandrade <andradejudyson@gmail.com>
2025-09-13 13:51:10 -0300

    Atualizado

---
+++ questao13.c
@@ -0,0 +1,30 @@
+// Para o char x[4], em X[0] seu endereço de memoria
+// 4092
+
+// x + 1 = 4095
+// x + 2 = 4096
+// x + 3 = 4097
+
+
+// Para o int x[4], em X[0] seu endereço de memoria
+// 4092
+
+// x + 1 = 4094
+// x + 2 = 4096
+// x + 3 = 4098
+
+
+// Para o float x[4], em X[0] seu endereço de memoria
+// 4092
+
+// x + 1 = 4096
+// x + 2 = 4100
+// x + 3 = 4104
+
+// Para o double x[4], em X[0] seu endereço de memoria
+// 4092
+
+// x + 1 = 4100
+// x + 2 = 4108
+// x + 3 = 4116
+
