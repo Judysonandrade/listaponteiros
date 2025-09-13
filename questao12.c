63a343869269ff8d50a669be806ffc02b0468c67
Judysonandrade <andradejudyson@gmail.com>
2025-09-13 13:28:21 -0300

    Atualizado

--- questao12.c
+++ questao12.c
@@ -3,9 +3,9 @@
 int main( ){
     int const *x = 3; // uma variavel x declarada como um ponteiro inteiro constante 
     printf("%d", ++(*x)); // o (*x) vai acessar o endereço de memoria e vai acessar o conteudo que está nesse endereço 
-    // de memoria e isso ia incrementa em x = ADEF4(exemplo) + 4 ou seja ia ser ADEF8 mas como é constante
-    // a saida vai ser 3  
-
+    // de memoria e isso ia incrementa em 1 mas como é constante
+    // a saida vai ser 3 e o printf não vai funcionar por esse motivo não vai incrementa
+    
     return 0;
 
 }
